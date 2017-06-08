39a2473e0efeed2641e9d4d44677cb819b033db1
Kevin Amiranoff <kamiranoff@gmail.com>
2017-06-09 01:01:49 +0200

    refactor message

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const htpp = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const { generateMessage } = require('./utils/message');
+
 const app = express();
 // allow us to use socket.io
 const server = htpp.createServer(app);
@@ -13,29 +15,17 @@ const PORT = process.env.PORT || 3000
 const PUBLIC_PATH = path.join(__dirname, '../public');
 
 // express static middleware
-app.use(express.static(PUBLIC_PATH))
+app.use(express.static(PUBLIC_PATH));
 
 
 io.on('connection', (socket) => {
   console.log('new user connected');
 
-  socket.emit('newMessage', {
-    from:'Admin',
-    text: 'Welcome to the chat app',
-  });
-
-  socket.broadcast.emit('newUserJoined',{
-    from: 'Admin',
-    text: 'New user joined',
-
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  socket.broadcast.emit('newUserJoined', generateMessage('Admin','New user joined'));
 
   socket.on('createMessage', (message) => {
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
   });
 
   socket.on('disconnect', () => {
