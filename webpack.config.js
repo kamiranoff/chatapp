e458274cd310bdcb0ecfb7c8135bb04466d8a3ff
Kevin Amiranoff <kamiranoff@gmail.com>
2017-06-12 21:50:39 +0200

    fix build path

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
+
+console.log(path.resolve(__dirname + 'build'));
 const config = {
   entry: './public/index.js',
   output: {
-    path: path.resolve(__dirname + 'build'),
+    path: path.resolve(__dirname + '/build'),
     filename: 'bundle.js'
   }
 };
